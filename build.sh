3522e90d565595ffed90252c851db719c53aaf16
Gleb Liutsko <gleb@liutsko.ru>
2023-06-06 07:17:05 +0400

    shellcheck for build.sh

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ DOTNET_PUBLISH_OPTION="-c Release -p:PublishReadyToRun=true -f net7.0"
 DOTNET_PUBLISH_OPTION_SELF="$DOTNET_PUBLISH_OPTION -p:IncludeNativeLibrariesForSelfExtract=true --self-contained"
 DOTNET_PUBLISH_OPTION_NO_SELF="$DOTNET_PUBLISH_OPTION --no-self-contained"
 
-BASEDIR=$(realpath $(dirname $0))
+BASEDIR=$(realpath "$(dirname "$0")")
 BUILDDIR="$BASEDIR/build"
 
 PATH_TO_PROJECT_SERVER="$BASEDIR/src/ExamPapers.ServerAPI"
 
-cd $BASEDIR
+cd "$BASEDIR" || exit 1
 
 # Windows (Server)
 dotnet publish $PATH_TO_PROJECT_SERVER $DOTNET_PUBLISH_OPTION_SELF -r win-x64 -o "$BUILDDIR/windows-self"
